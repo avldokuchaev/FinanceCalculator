c0dda71babfdd695f87e8cbc8b64f2f932056961
Andrei Dokuchaev <a.vl.dokuchaev@gmail.com>
2021-07-19 22:10:47 +0300

    Credit Calculator Annuitet 2 version

--- Credit_Calculator_annuitet.py
+++ Credit_Calculator_annuitet.py
@@ -1,1 +1,12 @@
 # Кредитный калькулятор аннуитетный
+
+summa_zaima = float(input("Введите сумму займа: "))
+srok_kreditovaniya = int(input("Введите срок кредитования в месяцах: "))
+procentnaya_stavka = float(input("Введите процентную ставку: "))
+
+mes_procent_stavka = (procentnaya_stavka / 12) / 100
+chisl_annuitet = mes_procent_stavka * ((1 + mes_procent_stavka) ** srok_kreditovaniya)
+znamen_annuitet = ((1 + mes_procent_stavka) ** srok_kreditovaniya) - 1
+koefficient_annuiteta = chisl_annuitet / znamen_annuitet
+month_payment = summa_zaima * koefficient_annuiteta
+print(f"Сумма ежемесячного платежа = {round(month_payment, 2)}")
