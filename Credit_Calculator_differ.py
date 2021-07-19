d533eb8e435d75771d85c5cd82a6fbb56b49497d
Andrei Dokuchaev <a.vl.dokuchaev@gmail.com>
2021-07-19 23:38:27 +0300

    Credit Calculator Different 3 version

--- Credit_Calculator_differ.py
+++ Credit_Calculator_differ.py
@@ -5,3 +5,21 @@ srok_kreditovaniya = int(input("Введите срок кредитования
 procentnaya_stavka = float(input("Введите процентную ставку: "))
 perv_vznos = float(input("Введите сумму первоначального взноса, иначе введите \"0\": "))
 
+chast_osnovn_dolga = summa_zaima / srok_kreditovaniya
+print(chast_osnovn_dolga)
+
+if perv_vznos == 0:
+    for mes in range(1, srok_kreditovaniya + 1):
+        summa_procentov = summa_zaima * (procentnaya_stavka / 100) * 30 / 365
+        summa_platega_v_mesyac = chast_osnovn_dolga + summa_procentov
+        print(f"Сумма выплаты в {mes} месяц = {round(summa_platega_v_mesyac, 2)}")
+        summa_zaima = summa_zaima - chast_osnovn_dolga
+else:
+    summa_zaima_minus_vznos = summa_zaima - perv_vznos
+    chast_osnovn_dolga_minus_vznos = summa_zaima_minus_vznos / srok_kreditovaniya
+    for mes in range(1, srok_kreditovaniya + 1):
+        summa_procentov = summa_zaima_minus_vznos * (procentnaya_stavka / 100) * 30 / 365
+        summa_platega_v_mesyac = chast_osnovn_dolga_minus_vznos + summa_procentov
+        print(f"Сумма выплаты в {mes} месяц = {round(summa_platega_v_mesyac, 2)}")
+        summa_zaima_minus_vznos = summa_zaima_minus_vznos - chast_osnovn_dolga_minus_vznos
+
