ff3e2d5ce550c8dafdc598345982f68a278979f8
Andrei Dokuchaev <a.vl.dokuchaev@gmail.com>
2021-07-21 22:56:23 +0300

    Deposit with every month capitalisation 1 version

---
+++ vklad_kapitaliz_every_month.py
@@ -0,0 +1,15 @@
+# Калькулятор вклада с ежемесячной капитализацией
+
+try:
+    summa_vklada = float(input("Введите сумму вклада: "))
+    srok_vklada = int(input("Введите срок вклада в месяцах: "))
+    procentnaya_stavka = float(input("Введите процентную ставку: "))
+except:
+    print("Вы ввели не цифры!")
+else:
+    procentnaya_stavka_modific = procentnaya_stavka / 100
+    koefficient_dnei = procentnaya_stavka_modific / 12
+    zarabotan_summa_full = summa_vklada * (1 + koefficient_dnei) ** srok_vklada
+    zarabotan_summa_no_full = zarabotan_summa_full - summa_vklada
+    print(f"За {srok_vklada} месяца(ев) вы получите {round(zarabotan_summa_no_full, 2)}")
+    print(f"Итоговая сумма через {srok_vklada} месяца(ев) составит {round(zarabotan_summa_full, 2)}")
