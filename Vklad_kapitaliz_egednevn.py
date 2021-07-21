0c64ea18b729c718138f566adc2c468e5b3f3cbc
Andrei Dokuchaev <a.vl.dokuchaev@gmail.com>
2021-07-21 22:48:23 +0300

    Deposit with every day capitalisation 2 version

--- Vklad_kapitaliz_egednevn.py
+++ Vklad_kapitaliz_egednevn.py
@@ -2,7 +2,7 @@
 
 try:
     summa_vklada = float(input("Введите сумму вклада: "))
-    srok_vklada = int(input("Введите срок вклада в месяцах: "))
+    srok_vklada = int(input("Введите срок вклада в днях: "))
     procentnaya_stavka = float(input("Введите процентную ставку: "))
 except:
     print("Вы ввели не цифры!")
@@ -11,5 +11,5 @@ else:
     koefficient_dnei = procentnaya_stavka_modific / 365
     zarabotan_summa_full = summa_vklada * (1 + koefficient_dnei) ** srok_vklada
     zarabotan_summa_no_full = zarabotan_summa_full - summa_vklada
-    print(f"За {srok_vklada} месяца(ев) вы получите {round(zarabotan_summa_no_full, 2)}")
-    print(f"Итоговая сумма через {srok_vklada} месяца(ев) составит {round(zarabotan_summa_full, 2)}")
+    print(f"За {srok_vklada} дней вы получите {round(zarabotan_summa_no_full, 2)}")
+    print(f"Итоговая сумма через {srok_vklada} дней составит {round(zarabotan_summa_full, 2)}")
