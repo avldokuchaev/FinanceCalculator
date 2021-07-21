f63e78a580cb2a21af2c4d82e9c9600e431e0828
Andrei Dokuchaev <a.vl.dokuchaev@gmail.com>
2021-07-21 22:49:29 +0300

    Deposit without capitalisation 3 version

--- Vklad_bez_capitalis.py
+++ Vklad_bez_capitalis.py
@@ -2,11 +2,11 @@
 
 try:
     summa_vklada = float(input("Введите сумму вклада: "))
-    srok_vklada = int(input("Введите срок вклада в месяцах: "))
+    srok_vklada = int(input("Введите срок вклада в днях: "))
     procentnaya_stavka = float(input("Введите процентную ставку: "))
 except:
     print("Вы ввели не цифры!")
 else:
     zarabot_summa_v_god = (summa_vklada * procentnaya_stavka * srok_vklada / 365) / 100
-    print(f"За {srok_vklada} месяца(ев) вы получите {round(zarabot_summa_v_god, 2)}")
-    print(f"Итоговая сумма через {srok_vklada} месяца(ев) составит {round(zarabot_summa_v_god + summa_vklada, 2)}")
+    print(f"За {srok_vklada} дней вы получите {round(zarabot_summa_v_god, 2)}")
+    print(f"Итоговая сумма через {srok_vklada} дней составит {round(zarabot_summa_v_god + summa_vklada, 2)}")
